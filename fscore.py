ba673995b460f03e3e0d11533144ec10c58e73be
tariklg <tarikglul@gmail.com>
2022-11-22 17:11:53 -0500

    v1.0

--- fscore.py
+++ fscore.py
@@ -3,29 +3,42 @@ import scipy.io as scio
 import sklearn.cluster as skc
 import sklearn.metrics as skm
 
-# load the array
+class KmeansAccuracy:
+    def __init__(self) -> None:
+        # load the data
 
-imIF = np.array(scio.loadmat("./062B6.mat")["imIF"])
-imIF = imIF.reshape((imIF.shape[0] * imIF.shape[1], imIF.shape[2]))
+        self.imIF = np.array(scio.loadmat("./062B6.mat")["imIF"])
+        self.imIF = self.imIF.reshape((self.imIF.shape[0] * self.imIF.shape[1], self.imIF.shape[2]))
 
-# kmeans = skc.KMeans(n_clusters=4, init="k-means++").fit(imIF)
+        # load ground truths
 
-# load ground truths
+        self.imL = np.array(scio.loadmat("./062B6_gt.mat")["imL"])
+        self.imL = self.imL.reshape(self.imL.shape[0] * self.imL.shape[1])
 
-imL = np.array(scio.loadmat("./062B6_gt.mat")["imL"])
-imL = imL.reshape(imL.shape[0] * imL.shape[1])
+        # do kmeans clustering
+ 
+        self.kmeans = skc.KMeans(n_clusters=2, init="k-means++").fit(self.imIF)
+
+    # group labels with each other based and rename classes into indices and labels into colums
+
+    def grouping(a: list) -> list:
+        out = []
+        for point in a:
+            out.append([a.index(p) for p in a if p==point])
+            a = [i for i in a if i!=point]
+        return out
+
+    # calculate f1scores
+
+    def calculate(pred: list, true: list) -> list:
+        out = []
+        for pred_label in pred:
+            out.append([skm.f1_score(true_label, pred_label, average="macro") for true_label in true])
 
 
-# group the ground truths
+ka = KmeansAccuracy()
 
-def grouping(a: list) -> list:
-    out = []
-    for point in a:
-        out.append([a.index(p) for p in a if p==point])
-        a = [i for i in a if i!=point]
-    return out
+grouped_gt = ka.grouping(ka.imL)
+grouped_predicted = ka.grouping(ka.kmeans)
 
-def calculate(pred: list, true: list) -> list:
-    out = []
-    for pred_label in pred:
-        out.append([skm.f1_score(true_label, pred_label, average="macro") for true_label in true])
+f1_scores = ka.calculate(grouped_predicted, grouped_gt)
